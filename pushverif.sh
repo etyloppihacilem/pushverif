d54f996e719ecd448c13879494bb28b0fc5e8fe0
hmelica <hippolytemelica@icloud.com>
2022-08-14 21:45:05 +0200

    Changement de langue et lien pour les etoiles

--- pushverif.sh
+++ pushverif.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 echo -e "\033[1;34;40m####################################################################
-# Ce script vous est proposé par\033[1;37;40m hmelica \033[1;34;40mpour tester votre code :) #
-# Utilisez le flag\033[0;37;40m -v \033[1;34;40mpour afficher les commandes utilisées.       #
-# Bon code !                                                       #
-# N'hesitez pas à partager ce repository, mais il est important    #
-# de bien le cloner pour pouvoir bénéficier des mises à jour       #
-# automatiques !                                                   #
-# TIP: pour le partager rapidement, essayez le flag\033[0;37;40m -p \033[1;34;40m            #
+# This script was written by\033[1;37;40m hmelica \033[1;34;40mto test your code :)          #
+# Use the\033[0;37;40m -v \033[1;34;40mflag to enable verbose and see used commands.         #
+# Happy coding !                                                   #
+#                                                                  #
+# If you like the script, use the\033[0;37;40m -r \033[1;34;40mflag to give the repo a star  #
+# on github !                                                      #
+#                                                                  #
+# Do not hesitate to share this repository, but remember using the #
+# git link to enable auto-updates.                                 #
+#                                                                  #
+# TIP: to quickly share the script, use the\033[0;37;40m -p \033[1;34;40mflag :)             #
 ####################################################################\033[0m\n"
 
 verbose=0
-while getopts "vup" opt; do
+while getopts "vupr" opt; do
 	case $opt in
 		v)
 			verbose=1
@@ -28,7 +32,10 @@ while getopts "vup" opt; do
 	\033[36;40mgit clone https://github.com/etyloppihacilem/pushverif.git\033[0m"
 			exit
 			;;
-			
+		r)
+			x-www-browser https://github.com/etyloppihacilem/pushverif
+			exit
+			;;
 	esac
 done
 
